cad2c20ec207fec41a7e91d712d1c6511b55d88b
Adalida-Lewis <42812369+Adalida-Lewis@users.noreply.github.com>
2020-03-24 13:25:27 -0500

    Local server.js
    Added local server information on server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log('Request made');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('This server is running on hostname ' + hostname + ' and on port ' + port + '. This was created by Adalida Lewis');
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http://${hostname}:{port}/');
+});
+
